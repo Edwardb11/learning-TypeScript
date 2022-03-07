686a048f0eb4c7ad000467a7d399fd6c46857217
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-07 08:20:31 -0400

    first commit

---
+++ HolaMundo/funciones.js
@@ -0,0 +1,18 @@
+"use strict";
+var saludo = "HolaMundo desde TypeScript";
+saludo = "cambio";
+var numero;
+numero = 10;
+var cualquiera;
+cualquiera = "cadena";
+cualquiera = 10;
+var PI_NUMERO = 3.14;
+function saludar() {
+  console.log(saludo);
+  console.log(numero);
+  console.log(PI_NUMERO);
+}
+saludar();
+
+// tsc -w
+// para automatico compliar los  cambios
